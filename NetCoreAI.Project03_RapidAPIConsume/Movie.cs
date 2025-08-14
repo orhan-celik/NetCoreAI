dc47898a3ccd138b2ff6a44f072386e191028a7e
orhan-celik <orhancelik@GMWIN000576>
2025-08-14 11:33:58 +0300

    Rapid API üzerinden netflix apisini kullanarak film bölümlerini çektik.

---
+++ NetCoreAI.Project03_RapidAPIConsume/Movie.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCoreAI.Project03_RapidAPIConsume
+{
+    public class Movie
+    {
+
+
+        public string seasonId { get; set; }
+
+        public List<Episode> Episodes { get; set; }
+
+        public class Episode
+        {
+            public string episodeId { get; set; }
+            public Availability availability { get; set; }
+            public int bookmarkPosition { get; set; }
+            public Contextualsynopsis contextualSynopsis { get; set; }
+            public int displayRuntime { get; set; }
+            public int runtime { get; set; }
+            public string title { get; set; }
+            public Summary summary { get; set; }
+            public Interestingmoment interestingMoment { get; set; }
+        }
+
+        public class Availability
+        {
+            public bool isPlayable { get; set; }
+            public string availabilityDate { get; set; }
+            public long availabilityStartTime { get; set; }
+            public object unplayableCause { get; set; }
+        }
+
+        public class Contextualsynopsis
+        {
+            public string text { get; set; }
+            public string evidenceKey { get; set; }
+        }
+
+        public class Summary
+        {
+            public string type { get; set; }
+            public int id { get; set; }
+            public bool isOriginal { get; set; }
+            public int idx { get; set; }
+            public int episode { get; set; }
+            public int season { get; set; }
+            public bool isPlayable { get; set; }
+        }
+
+        public class Interestingmoment
+        {
+            public _342X192 _342x192 { get; set; }
+        }
+
+        public class _342X192
+        {
+            public Webp webp { get; set; }
+        }
+
+        public class Webp
+        {
+            public string type { get; set; }
+            public Value value { get; set; }
+        }
+
+        public class Value
+        {
+            public int width { get; set; }
+            public int height { get; set; }
+            public string url { get; set; }
+            public string image_key { get; set; }
+        }
+
+    }
+}
---
+++ NetCoreAI.Project03_RapidAPIConsume/Program.cs
@@ -0,0 +1,35 @@
+﻿using NetCoreAI.Project03_RapidAPIConsume;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+List<Movie> seasons = new List<Movie>();
+
+var client = new HttpClient();
+var request = new HttpRequestMessage
+{
+    Method = HttpMethod.Get,
+    RequestUri = new Uri("https://netflix54.p.rapidapi.com/season/episodes/?ids=80077209%2C80117715&offset=0&limit=25&lang=tr"),
+    Headers =
+    {
+        { "x-rapidapi-key", "a62d3ea27fmshcc12485941ec67ep1abb7djsndcae9fa54c91" },
+        { "x-rapidapi-host", "netflix54.p.rapidapi.com" },
+    },
+};
+using (var response = await client.SendAsync(request))
+{
+    response.EnsureSuccessStatusCode();
+    var body = await response.Content.ReadAsStringAsync();
+    seasons = JsonConvert.DeserializeObject<List<Movie>>(body)!;
+    foreach(var season in seasons)
+    {
+        Console.WriteLine($"Sezon : {season.Episodes[0].summary.season}");
+
+        foreach (var episode in season.Episodes)
+        {
+            Console.WriteLine($"Bölüm : {episode.summary.episode}, Film : {episode.title}");
+            Console.WriteLine($"Açıklama : {episode.contextualSynopsis.text}");
+        }
+    }
+}
+
+Console.ReadLine();
