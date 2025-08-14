2ab1b87c7a6809499316a8d18af215eaaecd6af1
orhan-celik <orhancelik@GMWIN000576>
2025-08-14 16:17:41 +0300

    Open AI DALL-E ile metinden resim oluşturma

---
+++ NetCoreAI.Project06_OpenAI_DallEImageGeneration/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("DALL-E Image Generation Example");
+
+        var config = new ConfigurationBuilder()
+            .AddUserSecrets<Program>()
+            .Build();
+
+        var apiKey = config["ApiKey"];
+
+        Console.Write("Örnek prompt : ");
+        string prompt = Console.ReadLine();
+
+        using (HttpClient client = new HttpClient()) {
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+
+            var requestBody = new {
+                prompt = prompt,
+                n = 1,
+                size = "1024x1024"
+            };
+
+            string json = JsonConvert.SerializeObject(requestBody);
+            var content = new StringContent(json,System.Text.Encoding.UTF8,"application/json");
+
+            HttpResponseMessage response = await client.PostAsync("https://api.openai.com/v1/images/generations", content);
+            string responseContent = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine(responseContent);
+
+        }
+
+    }
+}
