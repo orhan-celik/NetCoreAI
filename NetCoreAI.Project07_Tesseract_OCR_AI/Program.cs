5315327069c46c29b6378ad902e0efcc78d940f7
orhan-celik <orhancelik@GMWIN000576>
2025-08-14 16:45:25 +0300

    Tesseract OCR ile Resim Üzerindeki karakterleri Okuma

---
+++ NetCoreAI.Project07_Tesseract_OCR_AI/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tesseract;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Tesseract OCR ile Resim Üzerindeki karakterleri Okuma");
+
+        try
+        {
+
+            Console.Write("Okunacak olan resmin yolunu girin : ");
+            string imagePath = Console.ReadLine() ?? "image1.png"; // Okunacak resim dosyasının yolu
+            string tessDataPath = @"C:\tessdata"; // Tesseract'ın dil verilerinin bulunduğu klasör
+            // Tesseract OCR motorunu başlatma
+            using (var engine = new TesseractEngine(tessDataPath, "eng", EngineMode.Default))
+            {
+                using (var img = Pix.LoadFromFile(imagePath))
+                {
+                    using (var page = engine.Process(img))
+                    {
+                        string text = page.GetText();
+                        Console.WriteLine("OCR Sonucu:");
+                        Console.WriteLine(text);
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Hata: {ex.Message}");
+        }
+    }
+}
