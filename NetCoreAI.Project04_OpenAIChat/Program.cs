74b0aab0996a46e259bf0e8db8446c44827a6f4d
orhan-celik <orhancelik@GMWIN000576>
2025-08-14 15:15:58 +0300

    Open AI ile Chat Apisi

---
+++ NetCoreAI.Project04_OpenAIChat/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenAI_API;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+
+        try
+        {
+            var config = new ConfigurationBuilder()
+                        .AddUserSecrets<Program>()   // Program sınıfının UserSecretsId'sini kullan
+                        .Build();
+
+            var apiKey = config["ApiKey"]; // UserSecrets'den API anahtarını al
+
+            OpenAIAPI api = new OpenAIAPI(apiKey);
+
+            var chat = api.Chat.CreateConversation();
+
+            //chat.Model = Model.GPT4_Turbo;
+
+            Console.WriteLine("Birşeyler sorun : ");
+            string prompt = Console.ReadLine();
+            
+            chat.AppendUserInput(prompt);
+
+            await foreach (var res in chat.StreamResponseEnumerableFromChatbotAsync())
+            {
+                Console.Write(res);
+            }
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        Console.ReadLine();
+        
+    }
+}
