ca6f52bd9bf7e5fb933505f740d730befeff93be
orhan-celik <orhancelik@GMWIN000576>
2025-08-15 09:21:59 +0300

    Google Vision API ile Resim üzaerindeli yazıyı analiz etme çalışması

---
+++ NetCoreAI.Project08_GoogleCloudVision_OCR/Program.cs
@@ -0,0 +1,37 @@
+﻿using Google.Cloud.Vision.V1;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Google Cloud Vision OCR ile Resim üzerindeki yazıları okuma");
+
+        Console.Write("Resim yolunu giriniz:");
+        string imagePath = Console.ReadLine() ?? "image1.png"; // Okunacak resim dosyasının yolu
+
+        Console.WriteLine();
+
+        string credentialPath = @"C:\Users\orhancelik\Desktop\my-ai-project-469105-05a07b5a8af1.json";
+        Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", credentialPath);
+
+        try
+        {
+            var client = ImageAnnotatorClient.Create();
+            var image = Image.FromFile(imagePath);
+            var response = client.DetectText(image);
+            Console.WriteLine("Resimdeki Metin:");
+            Console.WriteLine();
+            foreach (var annotination in response)
+            {
+                if (!string.IsNullOrEmpty(annotination.Description))
+                {
+                    Console.WriteLine(annotination.Description);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Bir hata oluştu {ex.Message}");
+        }
+    }
+}
